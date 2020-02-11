cc456ce8012cc05bce0e3a03cfdb567b84e3ff56
Innocent93 <kayamba.innocent885@gmail.com>
2020-02-12 00:51:54 +0200

    Update random.rb

--- Math/random.rb
+++ Math/random.rb
@@ -1,18 +1,18 @@
 #bts => behind the scene
 def rangebts
 	print 'Enter smallest number '
-	s = gets.chop().to_i
+	s = gets.chomp().to_i
 	print 'Enter biggest number '
-	b = gets.chop().to_i
+	b = gets.chomp().to_i
 	puts 'Enter the number of elements to be sampled'
 	c = gets.chomp().to_i
 	re = [*s..b]
 	if s>b
-		return 'please start by entering the smallest number'
+    p 'please start by entering the smallest number'
 	elsif c < 0
-		return 'Only zero and positive numbers are allowed'
+    p 'Only zero and positive numbers are allowed'
 	else
-		return re.sample(c)
+    p re.sample(c)
 	end
 end
 
@@ -22,9 +22,9 @@ def range(a,b,c=1)
 	b.to_i
 	re = [*a..b]
 	if a>b
-		return 'please start by entering the smaller number'
+    p 'please start by entering the smaller number'
 	else
-		return re.sample(c)
+    p re.sample(c)
 	end
 end
 
