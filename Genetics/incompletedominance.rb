bd01cdc9630635a57e44065afe7a31c0a879799b
Innocent93 <kayamba.innocent885@gmail.com>
2020-03-24 15:38:16 +0200

    minor

--- Genetics/incompletedominance.rb
+++ Genetics/incompletedominance.rb
@@ -1,21 +1,20 @@
-#Lethal gene (Pieotropy: An extension of mendelian fashion way of inheritance)
-def lethalgene(a,b)
+#Incomplete dominance
+def incompletedominance(a,b)
   genotype_1 = a.to_s
   genotype_2 = b.to_s
   puts "Enter recessive trait"
   rt = gets.chop
-  puts 'Enter lethal trait'
+  puts 'Enter intermediate trait'
   it= gets.chop
   puts 'Enter Dominant trait'
   dt = gets.chop
-  return 'The parent cannot possess homozygous recessive trait' if genotype_1 == genotype_1.downcase or genotype_2 == genotype_2.downcase
   segregation_1 = genotype_1.chars
   segregation_2 = genotype_2.chars
   offspring_1 = segregation_1[0] + segregation_2[0]
   offspring_2 = segregation_1[0] + segregation_2[1]
   offspring_3 = segregation_1[1] + segregation_2[0]
   offspring_4 = segregation_1[1] + segregation_2[1]
-  phenotype = {-1=>"Dies #{rt}", 0 =>"Heterozygous dominant #{it}", 1 =>"Homozygous dominant #{dt}"}
+  phenotype = {-1=>"Homozygous recessive #{rt}", 0 =>"Heterozygous incomplete-dominance #{it}", 1 =>"Homozygous Dominant #{dt}"}
   return 'Please, the limit number of alleles is two on each side' unless (a and b).size == 2
   return 'Please, enter two alleles on both sides of each parent gametes' unless a.size == b.size
   return 'please, both letters should be identical on both parents' unless a.downcase == b.downcase
@@ -54,7 +53,7 @@ def lethalgene(a,b)
   end
 end
 
-def rlethalgene(a,b)
+def rincompletedominance(a,b)
   te = genotype_1 = a.to_s.swapcase, genotype_2 = b.to_s.swapcase
-  lethalgene(te[0],te[1])
+  incompletedominance(te[0],te[1])
 end
