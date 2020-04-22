8985ba091a8f2c6ddb98eff095efedc1d71ee47a
Innocent93 <kayamba.innocent885@gmail.com>
2020-04-22 22:49:45 +0200

    minutes
    on genetics only

---
+++ Genetics/phosphate.rb
@@ -0,0 +1,16 @@
+def phos(b)
+  seq = b.upcase.chars.reject{|s|s != ?C and s != ?A and s != ?U and s != ?G}
+  return "No Phosphates formed" if seq.empty? == true
+  puts String((seq.count - 1)).rjust(4)
+  puts               "O".rjust(4)
+          puts       "|".rjust(4)
+  puts  "-O"+"—"+"P"+"—"+"0"
+          puts       "|".rjust(4)
+          puts       "O".rjust(4)
+
+rescue NoMethodError
+  puts "No phosphate formed"
+
+
+
+end
