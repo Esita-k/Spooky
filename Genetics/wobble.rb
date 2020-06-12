66b7f6eaebfc77ff120d97bd58e82d6b86c937b3
Innocent93 <kayamba.innocent885@gmail.com>
2020-06-12 03:14:35 +0200

    little

--- Genetics/wobble.rb
+++ Genetics/wobble.rb
@@ -3,17 +3,22 @@ def wobble(b)
   return "only triplets codon allowed" if acodon.count > 3 or acodon.count < 3
 
   if acodon.last == "C" or  acodon.last == "A"
-    base = {:A => 'T', :G => 'C', :T => 'A', :C =>'G'}
+    base = {:A => 'U', :G => 'C', :U => 'A', :C =>'G'}
     puts "5'-" + final = acodon.count.times.map{|y|base[(acodon[y]).to_sym]}.join + "-3'"
     elsif acodon.last == "U"
-    base = {:A => 'T', :G => 'C', :T => 'A', :C =>'G'}
+    base = {:A => 'U', :G => 'C', :U => 'A', :C =>'G'}
     final = acodon.first(2).count.times.map{|y|base[(acodon[y]).to_sym]}
     u = %w(A G).sample(1)
     puts "5'-" + (final << u[0]).join + "-3'"
-  elsif acodon.last == "G" or acodon.last == "I"
-    base = {:A => 'T', :G => 'C', :T => 'A', :C =>'G'}
+  elsif acodon.last == "G" 
+    base = {:A => 'U', :G => 'C', :U => 'A', :C =>'G'}
     final = acodon.first(2).count.times.map{|y|base[(acodon[y]).to_sym]}
-    u = %w(U C A).sample(1)
+    u = %w(U C).sample(1)
+    puts "5'-" + (final << u[0]).join + "-3'"
+  elsif acodon.last== "I"
+ 	base = {:A => 'U', :G => 'C', :U => 'A', :C =>'G'}
+    final = acodon.first(2).count.times.map{|y|base[(acodon[y]).to_sym]}
+    u = %w(A U C).sample(1)
     puts "5'-" + (final << u[0]).join + "-3'"
   else
     puts "Gene error"
