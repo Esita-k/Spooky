a04a4baf3aa71a2634803a2dedd304b308e2534a
Esita Kayamba <38175215+Esita-k@users.noreply.github.com>
2020-07-19 05:28:52 +0200

    with folder now

---
+++ Genetics/Mendelian Extension/nallele.rb
@@ -0,0 +1,4 @@
+#This formula is created for multiple alleles.
+def nllele(b)
+  Integer(b/2.0 * (b+1))
+end
--- Genetics/nallele.rb
+++
@@ -1,4 +0,0 @@
-#This formula is very important for multiple alleles.
-def nllele(b)
-  Integer(b/2.0 * (b+1))
-end
